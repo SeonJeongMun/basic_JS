dad099b1ced7411ca79f969690c27113d579824d
SeonJeongMun <inception010203@gmail.com>
2023-09-27 16:48:06 +0900

    JS 객체타입 object

--- main.js
+++ main.js
@@ -1,52 +1,38 @@
-// 자료형
-let str = "hello,world";
-console.log(typeof str);
+//객체타입 object - 다수의 원시 자료형 포함/복잡한 개체(entity)표현할 수 있는 자료형
+// Object() or ({})으로 생성. 개체는 key:value 형태. 접근은 object.key 형태
 
-console.log(typeof undefined);
-console.log(typeof 123);
-console.log(typeof 456n);
-console.log(typeof 123.123);
-console.log(typeof true);
-console.log(typeof "hello");
-console.log(typeof Symbol("id"));
-console.log(typeof Math);
-console.log(typeof null); //하위 버전 호환성으로 object 표기
-console.log(typeof console.log);
+let user = {
+    name:"john", //여기는 ,
+    age:27, 
+}; // 마지막에 ;
 
-//Boolean 논리적인 값 표현 true/false 두가지만 존재, 조건문 등에서 동작 판단 기준으로 사용
-let name_check = true;
-let age_check = false;
+console.log(typeof user);
+console.log(typeof user.name);
+console.log(typeof user.age);
 
-let value_check = 10 > 3;
-console.log(value_check);
+console.log(user);
+console.log(user.name); //console.log(user["name"]); 도 같음
+console.log(user.age);
 
-// null = 값이 비어있다. 존재하지 않는 비어있는 알 수 없는 값 / undefined =  할당되어 있지 않은 상태 변수선언 후 초기화 x면 undefined 자동할당
-const null_check = null;
-console.log(null_check === null); // output true 나오는데 단, const에서 null대신 123 같은거 할당해주면 여기 output은 false로 나옴
+user.age = 30;
+console.log(user.age); // 값 27에서 30으로 변경
 
-let A;
-console.log(A);
+//객체 내 entity 추가/삭제
+user.weight = 72; //weight 필드 추가
+console.log(user); //위의 console.log(user)와 달리 weight 추가 됨
 
+delete user.age;
+console.log(user); //여기서는 age 삭제된 상태로 나옴
 
-// number 정수, 부동소수점 숫자 표현. 사칙연산. Infinity, -Infinity, NaN(Not a Number)등 특수 숫자 값 포함. 2^53 - 1 보다 큰 수는 bigint 사용해야
-let num_1 = 123.0;
-let num_2 = 123.456;
-let num_3 = 1/0;
-let num_4 = 123456n;
+//객체 복사 문제점 - object값 복사시 대상 전체가 아닌 object내 주소값만 복사되기 때문 .  (user와 admin이 같은 주소값을 쓰니까 다 같이 바뀌는듯)
+// 가리키는 대상 전체 복사하려면 얕은 복사/깊은 복사 
+let admin = user;
+console.log(admin);
 
-console.log(num_1 - num_2); //-0.45600000000000307 오차 발생
-console.log((num_1 - num_2).toFixed(3)); // 소수점 세번째 자리수 밑으로는 반올림해라. (소수점 세번째 자리수까지 반올림?) -0.456
-console.log(num_3);
-console.log(num_1 / "hello");
-console.log(typeof num_4);
+admin.name = "park";
+console.log(admin.name); //park로 뜸
+console.log(user.name); //여기는 바꾸지않았는데 part로 바뀌어서 출력
 
-//string 문자, 문자열 표현 3가지 종류의 따옴표로 표현 가능 "",'', 역따옴표(tab키 위에 있음)
-let str_1 = "hello_1";
-let str_2 = 'hello_2';
-
-let num = 5;
-let str_3 = `hello_${num}`;
-
-console.log(str_1);
-console.log(str_2);
-console.log(str_3);
+user.weight  = 50;
+console.log(user.weight); //50으로 출력
+console.log(admin.weight); // 여기도 50으로 출력됨
