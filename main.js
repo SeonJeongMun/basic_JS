bb69a5256d5d9e769655c3f86fc0e2aa2e5c73c8
SeonJeongMun <inception010203@gmail.com>
2023-09-27 16:25:25 +0900

    JS - 자료형

--- main.js
+++ main.js
@@ -1,53 +1,52 @@
-/*
-// 1) 변수- 변경가능한 값을 저장하기 위한 기억 공간 (사용 전 반드시 선언 필요, 중복 선언 불가, 키워드 let)
-let A = 123;
+// 자료형
+let str = "hello,world";
+console.log(typeof str);
+
+console.log(typeof undefined);
+console.log(typeof 123);
+console.log(typeof 456n);
+console.log(typeof 123.123);
+console.log(typeof true);
+console.log(typeof "hello");
+console.log(typeof Symbol("id"));
+console.log(typeof Math);
+console.log(typeof null); //하위 버전 호환성으로 object 표기
+console.log(typeof console.log);
+
+//Boolean 논리적인 값 표현 true/false 두가지만 존재, 조건문 등에서 동작 판단 기준으로 사용
+let name_check = true;
+let age_check = false;
+
+let value_check = 10 > 3;
+console.log(value_check);
+
+// null = 값이 비어있다. 존재하지 않는 비어있는 알 수 없는 값 / undefined =  할당되어 있지 않은 상태 변수선언 후 초기화 x면 undefined 자동할당
+const null_check = null;
+console.log(null_check === null); // output true 나오는데 단, const에서 null대신 123 같은거 할당해주면 여기 output은 false로 나옴
+
+let A;
 console.log(A);
 
-A = 456;
-console.log(A);
 
-//let A = 789; syntax error
+// number 정수, 부동소수점 숫자 표현. 사칙연산. Infinity, -Infinity, NaN(Not a Number)등 특수 숫자 값 포함. 2^53 - 1 보다 큰 수는 bigint 사용해야
+let num_1 = 123.0;
+let num_2 = 123.456;
+let num_3 = 1/0;
+let num_4 = 123456n;
 
+console.log(num_1 - num_2); //-0.45600000000000307 오차 발생
+console.log((num_1 - num_2).toFixed(3)); // 소수점 세번째 자리수 밑으로는 반올림해라. (소수점 세번째 자리수까지 반올림?) -0.456
+console.log(num_3);
+console.log(num_1 / "hello");
+console.log(typeof num_4);
 
+//string 문자, 문자열 표현 3가지 종류의 따옴표로 표현 가능 "",'', 역따옴표(tab키 위에 있음)
+let str_1 = "hello_1";
+let str_2 = 'hello_2';
 
-// 2) 상수 - 변경 불가능한 값을 저장하기 위한 기억 공간 (사용 전 반드시 선언 필요,중복 선언 불가,키워드 const 상수는 보통 대문자로 표기)
-const B = 123; // 상수 선언과 동시에 값 초기화
-console.log(B);
+let num = 5;
+let str_3 = `hello_${num}`;
 
-// const B = 456; TypeError
-
-/* const C; 초기화 없이 상수 선언 => 에러
-C = 123; Syntax Error */
-
-
-/*
-// 3) 변수 / 상수 예제 코드
-let hi; // 선언 후 할당
-hi = "hello";
-
-let = "hello"; //선언과 동시에 초기화
-
-let name = "jeon", age = 13, msg = "hollo"; //한 줄에 여러 변수 선언하고 초기화
-
-const TESTCASE = 5; // 상수는 보통 대문자 표기
-const BIRTHDAY = "2020. 1. 5";
-
-console.log(age);
-console.log(BIRTHDAY); */
-
-
-// 4) 호이스팅 - 코드에 서넝ㄴ된 변수 및 함수를 유효한 범위의 코드 상단으로 끌어올리는 작업
-// var 변수/함수의 선언만 위로 올려지고, 할당은 올려지지 x
-// let/const 변수 선언과 함수 표현식에서는 호이스팅 발생 x
-
-console.log(name_1); //using var
-var name_1= "john";
-console.log(name_1);
-
-var name_2; //hosting 위 코드와 같음 
-console.log(name_2);
-name_2 = "john";
-console.log(name_2);
-
-console.log(name_3); // let 사용 바로 에러뜨기때문에 오류수정에 더 좋음
-let name_3 = "john"; 
+console.log(str_1);
+console.log(str_2);
+console.log(str_3);
