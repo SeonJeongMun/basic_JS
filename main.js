1006e71ecf5fd4aa042ad8d23421d78fd9bbfd9c
SeonJeongMun <inception010203@gmail.com>
2023-10-04 12:58:56 +0900

    JS 반복문

--- main.js
+++ main.js
@@ -1,32 +1,29 @@
-//조건문 switch
+//반복문 for
+/* 선언문, 조건문, 증감문 자리에 공백 입력 가능 조건문이 fail되기 전까지 코드블록 반복 수행
+for(선언문; 조건문; 증감문) {
+// statement Block
+} 형태  */
 
-const day = 3;
-let weekend = "";
-
-switch (day) {
-    case 1 :
-        weekend = "Monday";
-        break;
-    case 2 :
-        weekend = "Tuesday";
-        break;
-    case 3:
-        weekend = "Wednesday";
-        break;
-    case 4:
-        weekend = "Thursday";
-        break;
-    case 5:
-        weekend = "Friday";
-        break;
-    case 6:
-        weekend = "Saturday";
-        break;
-    case 7:
-        weekend = "Sunday";
-        break;
-    /* 그 이외의 값은 어떻게 하라는 것이 없었으므로 안해도 됨 default:
-        weekend = "error";
-        break; */
+for (let i = 0; i < 3; i++) {
+    console.log(i); // 0,1,2
 }
-console.log(weekend);
+
+for (let i = 10; i < 3; i++) {
+    console.log(i); 
+} //수행되지 않음
+
+let num = 0;
+for (; num <2; ) {
+    console.log(num); //여기까지만 하면 0인 상태로 무한루프 계속 0 나옴
+    num++;
+} // 0 1
+
+// 이중 for문
+
+for (let i = 0; i <3; i++) {
+    for(let j= 0; j<3; j++) {
+        console.log(`${i}+ ${j} = ${i+j}`);
+    }
+}
+
+//for in for of  
