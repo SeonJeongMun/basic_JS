a77e9921b4d043f8e7323931d00050ec0d1908cb
SeonJeongMun <inception010203@gmail.com>
2023-10-04 10:29:32 +0900

    JS 산술 대입 연산자

--- main.js
+++ main.js
@@ -1,41 +1,25 @@
-//객체 복사 문제점 - object값 복사시 대상 전체가 아닌 object내 주소값만 복사되기 때문 .  (user와 admin이 같은 주소값을 쓰니까 다 같이 바뀌는듯)
-// 가리키는 대상 전체 복사하려면 얕은 복사/깊은 복사 
+//연산자 - 산술/대입연산자
 
+console.log(31+10-9); //덧셈,뺄셈 연산자
+console.log(31*10); //곱셈 연산자
+console.log(31/10); //나눗셈 연산자
+console.log(parseInt(31/10)); //나눗 셈 몫 구하기
+console.log(31%10); //나머지 연산자 %
+console.log(2**3); //거듭제곱 연산자
 
-let user = {
-    name:"john", 
-    age:27, 
-    sizes: {
-        height: 100,
-        weight: 72,
-    }
-}; 
+//대입연산자
+let num_1 = 123;
+let num_2 = 456;
+let str_1 = "hello";
+let str_2 = "wordl";
 
-/* 얕은 복사 문제점 - 객체 내 또 다른 객체가 있다면 복사되지 않음
-얕은 복사 1 for loop 이용
-let admin ={};
-for (let key in user) {
-    admin[key] = user[key];
-} 
- 얕은 복사 2 
-let admin = Object.assign({},user);
+let num_3, str_3;
 
- 얕은 복사 3
-let admin = {...user};  => {user.name, user.age} user내의 필드값 전개시켜라
-*/
+num_3 = num_1 + num_2;
+str_3 = str_1 + str_2; //문자열 연결도 산술연산으로 가능
 
-//깊은 복사 JSON 사용 나중에 다시 check
-let admin = JSON.parse(JSON.stringify(user));
+console.log(num_3);
+console.log(str_3);
 
-
-admin.sizes.height = admin.sizes.height+1; //100 =>101
-console.log(admin.sizes.height); //101
-console.log(user.sizes.height); //101
-
-admin.name = "park";
-console.log(admin.name); 
-console.log(user.name); 
-
-user.age = 50;
-console.log(user.age); 
-console.log(admin.age); 
+let num_4 = num_1 - num_2;
+console.log(num_4);
