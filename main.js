e813a9d2428eea9ef1c0553e4a5dc08845984669
SeonJeongMun <inception010203@gmail.com>
2023-10-04 13:09:01 +0900

    JS 반복문 while

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
-//반복문 for
-/* 선언문, 조건문, 증감문 자리에 공백 입력 가능 조건문이 fail되기 전까지 코드블록 반복 수행
-for(선언문; 조건문; 증감문) {
-// statement Block
-} 형태  */
+//반복문 while
+//for 문에 비해 선언문과 증감문 없이 루프 수행, 무한루프 수행시 많이 사용
+// do while은 최소 한번 수행 필요할 때 사용
 
-for (let i = 0; i < 3; i++) {
-    console.log(i); // 0,1,2
+
+/* let i =0;
+
+while(i < 3) {
+    console.log(i); //여기까지 하면 무한 루프
+    i++;
 }
 
-for (let i = 10; i < 3; i++) {
-    console.log(i); 
-} //수행되지 않음
+i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i <3);
+ */
 
-let num = 0;
-for (; num <2; ) {
-    console.log(num); //여기까지만 하면 0인 상태로 무한루프 계속 0 나옴
-    num++;
-} // 0 1
+i = 4;
+do {
+    console.log(i);
+    i++;
+} while (i <3);
+//조건이 false임에도 먼저 한번은 console 하기 때문에 4 나옴
 
-// 이중 for문
+let j = 5;
 
-for (let i = 0; i <3; i++) {
-    for(let j= 0; j<3; j++) {
-        console.log(`${i}+ ${j} = ${i+j}`);
-    }
+while(j < 3) {
+    console.log(j); 
+    j++;
 }
-
-//for in for of  
+//조건이 fasle라서 수행 x
