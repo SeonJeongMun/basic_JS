c04e3365377379d4a006208e4b2d6c8ef5d29254
SeonJeongMun <inception010203@gmail.com>
2023-10-05 11:15:36 +0900

    JS 배열

--- main.js
+++ main.js
@@ -1,54 +1,31 @@
-//함수 정의 - 방식 3가지
-/* 1. 함수 선언식
-function func(arg1. arg2... argN) {
-    expression;
-} */
-function add(x,y) {
-    return x + y;
-}
+//배열 
+// 배열 크기 및 배열 여부 확인: Array.length, Array.isArray()
+// 배열 추가/삭제: Array.push(), Array.pop(), Array.shift(), Array.unshift(), Array.splice(), Array.slice() 등
+// 배열탐색: Array.indexOf(), Array.lastIndexOf(),Array.includes()
+// 배열변형(callback미사용): Array.sort(), Array.reverse(), Array.join()
+// 배열선언방법 
+let arr_1 = new Array(10); //사이즈 지정가능
+let arr_2 = [];
 
-/* 2. 함수 표현식
-const func = function (arg1, arg2, ... argN) {
-    expression;
-} */
+let fruits = ["apple", "orange", "melon"];
+console.log(fruits.length);
+console.log(fruits[0]);
 
-const add2 = function(x,y) {
-    return x+y;
-};
+fruits[1] = "kiwi";
+console.log(fruits);
 
-/* 3. 화살표 함수 
-const func = (arg1, arg2... argN) => expression;
-*/
+//배열타입확인
+let num = 123.456;
+let str = "hello";
+let fruit = ["apple", "orange", "banana"];
 
-const add3 = (x,y) => x+y;
+console.log(Array.isArray(num));
+console.log(Array.isArray(str));
+console.log(Array.isArray(fruit));
 
-//함수 호출
-function print_add(x,y) {
-    console.log(x+y);
-}
+console.log(fruit.length);
 
-print_add(); //NaN
-print_add(10); //NaN
-print_add(10,20); //30
-print_add(10,20,30); //30 뒤에 값은 무시됨
-
-function print_add2(x,y = 10) {
-    console.log(x+y);
-}
-print_add2(10); //y가 undefined지만 기본값이 10이므로 10 +10 = 20
-print_add2(10,20); //10+20=30
-
-//다이나믹 파라미터
-function print_min(){
-    console.log(arguments[0]- arguments[1]);
-    //arguments까지만 하면 [Arguments] { '0': 10, '1': 20 } 이렇게 나옴
-}
-print_min(10,20,30); //첫번째 원소 10 -두번째 원소 20 = -10인듯
-
-
-function ADD(x,y) {
-    return x+y; //break 역할
-    console.log("hello"); //이미 break했으므로 실행x 실행되게 하려면 return 위로 가야
-}
-let result = ADD(10,20);
-console.log(result);
+// delete로 삭제할 수 있으나 배열 사이즈가 그대로. 그 부분만 비게 됨. 잘 사용  x
+delete fruit[1];
+console.log(fruit);
+console.log(fruit.length);
